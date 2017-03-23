accbab39ac07b631e3484f0315bb73bfd7776954
Sagar Dalwale <sgrsad@gmail.com>
2017-03-23 17:01:13 +0530

    syntax changes in SagarGaneshLibrary.swift

--- SagarGaneshLibrary/Classes/SagarGaneshLibrary.swift
+++ SagarGaneshLibrary/Classes/SagarGaneshLibrary.swift
@@ -3,8 +3,9 @@ import UIKit
 public class BlinkingLabel: UILabel {
     
     public func startBlinking() {
-        let options : UIViewAnimationOptions = .Repeat
-        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+        let options : UIViewAnimationOptions = .repeat
+        UIView.animate(withDuration:
+            0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
         }, completion: nil)
     }
